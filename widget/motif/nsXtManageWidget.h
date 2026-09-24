cdcfbb962ec8a8f90a3f2514bdce42acde25ece4
db2k <33785843+gazecube@users.noreply.github.com>
2026-09-24 15:10:15 -0700

    motif: port original Mozilla manager widget

---
+++ widget/motif/nsXtManageWidget.h
@@ -0,0 +1,25 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/*
+ * Ported from Mozilla 0.9 widget/src/motif/nsXtManageWidget.h.
+ * Original code Copyright (C) 1998 Netscape Communications Corporation.
+ */
+
+#ifndef nsXtManageWidget_h__
+#define nsXtManageWidget_h__
+
+#include <Xm/Xm.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern WidgetClass newManageClass;
+
+typedef struct _NewManageClassRec* NewManageClass;
+typedef struct _NewManageRec* NewManage;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* nsXtManageWidget_h__ */
